81b360287ae6d3d63b5ee6ddf34cfcebae516e1e
JESMEX <str82jes@gmail.com>
2022-09-20 22:23:00 -0700

    3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-    if (i != 101 and i != 113):
-        print("{}".format(chr(i)), end="")
+"""
+Print the ASCII alphabet in lowercase, not followed by a new line.
+"""
+for letter in range(97, 123):
+    print("{}".format(chr(letter)), end="")
