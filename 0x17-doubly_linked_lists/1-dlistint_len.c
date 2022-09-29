9d10e32ec63a0d544861c96a24f657a32608f20b
JESMEX <str82jes@gmail.com>
2022-09-29 07:49:26 -0700

    doubly_linked_lists

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,18 +1,25 @@
+/*
+ * File: 1-dlistint_len.c
+ * Auth: Brennan D Baraban
+ */
+
 #include "lists.h"
+
 /**
- * dlistint_len - show us the number of elements in a DLL
- * @h: a pointer to the head of the DLL
- * Return: the number of elements in a DLL
+ * dlistint_len - Counts the number of elements in a linked dlistint_t list.
+ * @h: The head of the dlistint_t list.
+ *
+ * Return: The number of elements in the dlistint_t list.
  */
 size_t dlistint_len(const dlistint_t *h)
 {
-	size_t num_of_elements;
+	size_t nodes = 0;
 
-	num_of_elements = 0;
 	while (h)
 	{
+		nodes++;
 		h = h->next;
-		num_of_elements = num_of_elements + 1;
 	}
-	return (num_of_elements);
+
+	return (nodes);
 }
